09750388665822d40f2c1e9b4ca13c20b5629d02
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-03-04 11:53:03 -0600

    Removed changes from test run
    Deleted yaml
    Deleted test file

--- task_manager/test_task_manager.py
+++
@@ -1,9 +0,0 @@
-from task_manager.task_manager import TaskManager
-from task_manager.task import Task
-
-def test_add_task():
-    manager = TaskManager()
-    task = Task(description="Test Task")
-    manager.add_task(task)
-    tasks = manager.list_tasks()
-    assert task in tasks
