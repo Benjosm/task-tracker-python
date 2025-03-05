a1c06392ff97c90257988f76877533d23c735f53
Benjosm <92320536+Benjosm@users.noreply.github.com>
2025-03-05 13:32:19 -0600

    Add test case for task retrieval

--- task_manager/test_task_manager.py
+++ task_manager/test_task_manager.py
@@ -11,5 +11,14 @@ class TestTaskManager(unittest.TestCase):
         self.assertEqual(tasks[0]['title'], "Test Task")
         self.assertEqual(tasks[0]['description'], "Test Description")
 
+    def test_get_tasks(self):
+        task_manager = TaskManager()
+        task_manager.add_task("Task 1", "Description 1")
+        task_manager.add_task("Task 2", "Description 2")
+        tasks = task_manager.get_tasks()
+        self.assertEqual(len(tasks), 2)
+        self.assertEqual(tasks[0]['title'], "Task 1")
+        self.assertEqual(tasks[1]['title'], "Task 2")
+
     def test_minimal(self):
         self.assertTrue(True)
